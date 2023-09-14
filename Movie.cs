aaf04685e2d1c6c8a63e66a8ab0a4087a67294a3
Nomthandazo Ndweni <ndweni2658@gmail.com>
2023-09-14 11:52:51 +0200

    Add project files.

---
+++ Movie.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19.Setters_and_Getters
+{
+    internal class Movie
+    {
+        public string title;
+        public string director;
+        public string rating;
+
+        //constructor
+        public Movie(string aTitle, string aDirector, string aRating)
+        {
+            title = aTitle;
+            director = aDirector;
+            Rating =  aRating;  //now the movie rating is being set by the setter below
+        }
+
+        public string Rating 
+        { get 
+            { return rating; }
+            set
+            {
+                if (value == "G" || value == "PG" || value == "PG-13" || value == "R")
+
+                { rating = value;}
+                else { rating = "NR"; }
+            }
+        
+        }
+       
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19.Setters_and_Getters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie movie1 = new Movie("Titanic", "Jim", "PG-13");
+            Movie movie2 = new Movie("The unrealeased movie", "Ora", "Cat");
+
+
+            Console.WriteLine(movie1.title);
+            Console.WriteLine(movie1.director);
+            Console.WriteLine(movie1.rating);
+            Console.WriteLine(movie2.rating);
+            //freeze console
+            Console.ReadLine();
+        }
+    }
+}
